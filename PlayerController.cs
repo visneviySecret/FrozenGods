06484aab79503fe83ab83f76fe218b028caf97fc
visneviySecret <96699059+visneviySecret@users.noreply.github.com>
2024-06-14 21:54:22 +0300

    fix: jump once

--- PlayerController.cs
+++ PlayerController.cs
@@ -16,7 +16,10 @@ public class PlayerController : MonoBehaviour
     float jumpPower = 6;
     float jumpGravity = -35;
     float realGravity = -9.8f;
+    public float jumpForce = 0.125f;
+    private bool isGrounded;
     Coroutine movingCoroutine;
+
     void Start()
     {
         laneOffset = MapGenerator.instance.laneOffset;
@@ -25,19 +28,17 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) 
-        && pointFinish > -2*laneOffset)
+        if (Input.GetKeyDown(KeyCode.A) && pointFinish > -2 * laneOffset)
         {
             MoveHorizontal(-laneChangeSpeed);
         }
-        if (Input.GetKeyDown(KeyCode.D)
-         && pointFinish < 2*laneOffset)
+        if (Input.GetKeyDown(KeyCode.D) && pointFinish < 2 * laneOffset)
         {
             MoveHorizontal(laneChangeSpeed);
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
-            MoveLinear();
+            Jump();
         }
     }
 
@@ -47,62 +48,73 @@ public class PlayerController : MonoBehaviour
         pointFinish += Mathf.Sign(speed) * laneOffset;
 
         if (!isMoving)
-        {  
+        {
             StartCoroutine(MoveCoroutine(speed));
         }
-
         // if (isMoving) { StopCoroutine(movingCoroutine); isMoving = false; }
         // movingCoroutine = StartCoroutine(MoveCoroutine(speed));
     }
+
     void MoveLinear()
-    {   
+    {
         AccelerateSpeed();
-        if (!isJumping) Jump();
+        if (!isJumping)
+            Jump();
     }
+
     IEnumerator MoveCoroutine(float vectorX)
     {
         isMoving = true;
-        if (!isJumping) Jump();
+        if (!isJumping)
+            Jump();
         while (Mathf.Abs(pointStart - transform.position.x) < laneOffset)
         {
-            yield return new WaitForFixedUpdate(); 
+            yield return new WaitForFixedUpdate();
             rb.velocity = new Vector3(vectorX, rb.velocity.y, 0);
-            float x = Mathf.Clamp(transform.position.x, Mathf.Min(pointStart, pointFinish), Mathf.Max(pointStart, pointFinish));
+            float x = Mathf.Clamp(
+                transform.position.x,
+                Mathf.Min(pointStart, pointFinish),
+                Mathf.Max(pointStart, pointFinish)
+            );
             transform.position = new Vector3(x, transform.position.y, transform.position.z);
         }
         rb.velocity = Vector3.zero;
         transform.position = new Vector3(pointFinish, transform.position.y, transform.position.z);
         isMoving = false;
     }
-    void Jump() 
+
+    private void OnCollisionEnter(Collision collision)
     {
-        if (isJumping) return;
-        isJumping = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
         rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
-        Physics.gravity = new Vector3(0, jumpGravity, 0);
-        StartCoroutine(StopJumpCoroutine());
     }
-    IEnumerator StopJumpCoroutine()
-    {
-        isJumping = true;
-        yield return new WaitUntil(() => IsGrounded());
-        Physics.gravity = new Vector3(0, realGravity, 0);
-        RoadGenerator.Instance.speed = RoadGenerator.Instance.maxSpeed;
-        isJumping = false;
-        
-        // yield return new WaitUntil(() => IsGrounded());
-        // Physics.gravity = new Vector3(0, realGravity, 0);
-        // RoadGenerator.Instance.speed = RoadGenerator.Instance.maxSpeed;
-        // isJumping = false;
-    }
+
     bool IsGrounded()
     {
         return transform.position.y == 0;
     }
+
     void AccelerateSpeed()
     {
         RoadGenerator.Instance.speed += accelerateSpeed * Time.deltaTime;
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Enemy")
@@ -110,6 +122,7 @@ public class PlayerController : MonoBehaviour
             rb.constraints |= RigidbodyConstraints.FreezePositionZ;
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Enemy")
